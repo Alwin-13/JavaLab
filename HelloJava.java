05e7cec7de9da627a566bd33c862e0dfd1c78ffb
Alwin-13 <145443630+Alwin-13@users.noreply.github.com>
2023-09-19 15:05:33 +0530

    My first java project

---
+++ HelloJava.java
@@ -0,0 +1,17 @@
+/*
+* File         :  HelloJava.java
+* Description  :  My First Java Program 
+* Author       :  Alwin
+* Version      :  1.0
+* Date         :  19/09/2023
+*/
+
+public class HelloJava 
+{
+
+	public static void main(String [] args)
+	{
+	System.out.println("Hello");
+	System.out.print("Java");
+    }
+}
